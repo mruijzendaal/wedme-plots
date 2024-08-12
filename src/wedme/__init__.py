b1b9b6e6b8303490196df2b421de093095160d9d
Martijn Ruijzendaal <martijn@evfinder.nl>
2024-08-12 10:25:33 +0200

    Add some utilities to wedme.util

--- src/wedme/__init__.py
+++ src/wedme/__init__.py
@@ -8,10 +8,10 @@ Import the `wedme` module to apply styles:
 """
 
 # Standard library imports
+import wedme.util as util
 from wedme.util import imshow, colorbar, get_colormap_norm, unique_legend
 from wedme.styledefs import reset, dev, paper, poster, slide, thesis, a0
 from wedme.shorthands import figure, subplots, apply
 from wedme.const import *
-
 from wedme.gif import GifMaker
 from wedme import scale
--- src/wedme/util.py
+++ src/wedme/util.py
@@ -3,7 +3,7 @@ import matplotlib as _mpl
 import numpy as _np
 
 
-def colorbar(mappable=None, label="", labelpad=10, labelkwargs={}, **kwargs):
+def colorbar(mappable=None, label="", labelpad=8, labelkwargs={}, **kwargs):
     cax = _plt.colorbar(mappable=mappable, **kwargs)
     cax.set_label(label, rotation=270, labelpad=labelpad, **labelkwargs)
     return cax
@@ -25,13 +25,18 @@ def imshow(
     return _plt.imshow(data, *args, vmin=vmin, vmax=vmax, *args, **kwargs)
 
 
-def get_colormap_norm(cmap="viridis", X=None, min=None, max=None):
+def get_colormap_norm_standalone(cmap="viridis", X=None, min=None, max=None):
     if not X is None:
         min = _np.nanmin(_np.array(X))
         max = _np.nanmax(_np.array(X))
 
-    cmap = _plt.cm.get_cmap(cmap)
     norm = _mpl.colors.Normalize(vmin=min, vmax=max)
+    return norm
+
+
+def get_colormap_norm(cmap="viridis", X=None, min=None, max=None):
+    norm = get_colormap_norm_standalone(cmap, X, min, max)
+    cmap = _plt.cm.get_cmap(cmap)
 
     return lambda x: cmap(norm(x))
 
@@ -68,3 +73,53 @@ def unique_legend(ax=None, **kwargs):
     unique_labels, unique_handles = _np.unique(labels, return_index=True)
     unique_handles = [handles[i] for i in unique_handles]
     return _plt.legend(unique_handles, unique_labels, **kwargs)
+
+
+def arrow(
+    x,
+    y,
+    dx,
+    dy,
+    *args,
+    ax=None,
+    width_ratio=0.01,
+    head_aspect_ratio=1.5,
+    head_width_relative=3,
+    length_includes_head=True,
+    **kwargs,
+):
+    if ax == None:
+        ax = _plt.gca()
+
+    Dx = _np.ptp(ax.get_xlim())
+    Dy = _np.ptp(ax.get_ylim())
+    Wx = width_ratio * Dx
+    Wy = width_ratio * Dy
+
+    aspect_ratio = Dy / Dx
+
+    arrow_angle = _np.arctan2(dy, dx)
+
+    arrow_width = _np.sqrt(
+        (_np.cos(arrow_angle) * Wy) ** 2 + (_np.sin(arrow_angle) * Wx) ** 2
+    )
+    head_width = arrow_width * head_width_relative
+
+    head_length_ratio = width_ratio * head_width_relative * head_aspect_ratio
+    head_length = _np.sqrt(
+        (_np.cos(arrow_angle) * Dx * head_length_ratio) ** 2
+        + (_np.sin(arrow_angle) * Dy * head_length_ratio) ** 2
+    )
+
+    ax.arrow(
+        x,
+        y,
+        dx,
+        dy,
+        *args,
+        width=arrow_width,
+        head_width=head_width,
+        head_length=head_length,
+        length_includes_head=length_includes_head,
+        **kwargs,
+    )
